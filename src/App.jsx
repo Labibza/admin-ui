e3e363b93c2a164d9e221cd4e30150f3d7337fd1
Labibza <111202315339@mhs.dinus.ac.id>
2025-10-14 06:26:04 +0700

    menggunakan variant class tailwindcaa

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const courses = [
+    {
+      title: "System Administration and IT Infrastructure Services",
+    },
+    {
+      title: "Operating Systems Becoming a Power User",
+    },
+    {
+      title: "The Bits and Bytes of Computer Networking",
+    },
+    {
+      title: "Technical Support Fundamentals",
+    },
+    {
+      title: "How to Succeed at: Writing Applications",
+    },
+    {
+      title: "Medicine Administration for Carers",
+    },
+  ];
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-neutral-100 ">
-      <div className="flex bg-white rounded-lg shadow-lg overflow-hidden max-w-4xl">
-        <div className="w-120 h-100 bg-neutral-300 flex items-center justify-center">
-          <span className="text-neutral-400 text-7xl font-semibold">600 x 500</span>
-        </div>
-        <div className="w-120 h-100 p-10">
-          <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
-          <form className="space-y-5">
-            <div>
-              <label className="block text-sm font-medium text-neutral-700 mmb-1">
-                Email
-              </label>
-              <input type="email" 
-                placeholder="Masukkan Email"
-                className="w-full px-3 py-2 border border-neutral-300 rounded-md"
-              />
+    <>
+      <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-6">
+        {courses.map((course, index) => (
+          <div
+            key={index} className="shadow rounded-lg overflow-hidden flex flex-col
+            hover:border hover:border-gray-500
+            hover:shadow-lg hover:scale-105 transition-transform duration-300"
+          >
+            {/* Image */}
+            <img
+              src="https://www.placehold.co/300x200"
+              alt={course.title}
+              className="w-full h-44 object-cover"
+            />
+
+            {/* Content */}
+            <div className="p-4 bg-red-100 flex flex-col justify-between h-full">
+              <h3 className="text-lg font-semibold mb-4">{course.title}</h3>
+
+              <div className="bg-red-50 p-2 rounded-lg">
+                <div className="text-sm text-gray-500 mt-2 flex justify-between">
+                  <span>👥 123 users</span>
+                  <span>⏱ 60 min</span>
+                </div>
+
+                {/* Author */}
+                <div className="mt-3 mb-2 flex items-center gap-2">
+                  <img
+                    src="https://www.placehold.co/50x50"
+                    alt="Author's Avatar"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                  <div>
+                    <p className="text-sm font-medium">Author's Name</p>
+                    <p className="text-xs text-gray-500">Designer</p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Footer */}
+              <div className="mt-4 flex items-center justify-between">
+                <div className="bg-white py-2 px-4 rounded">$123</div>
+                <button 
+                  className="bg-red-700 hover:bg-red-900 active:bg-red-500
+                  text-white px-4 py-2 rounded text-sm">
+                  Get Started
+                </button>
+              </div>
             </div>
-            <div>
-              <label className="block text-sm font-medium text-neutral-700 mmb-1">
-                Password
-              </label>
-              <input type="password" placeholder="Masukkan Password" className="w-full px-3 py-2 border border-neutral-300 rounded-md"
-              />
-            </div>
-            <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-md">Login</button>
-          </form>
-        </div>
+          </div>
+        ))}
       </div>
-    </div>
-    
-    // <>
-    //   <div className="min-h-screen bg-neutral-800
-    //   flex flex-col items-center justify-center text-center">
-    //     <div className='flex justify-center items-center space-x-8 animate-bounce'>
-    //       <a href="https://vite.dev" target="_blank">
-    //         <img src={viteLogo} className="w-24 h-24" alt="Vite logo" />
-    //       </a>
-    //       <a href="https://react.dev" target="_blank">
-    //         <img src={reactLogo} className="w-24 h-24 animate-spin" alt="React logo" />
-    //       </a>
-    //     </div>
-    //     <h1 className='text-white  text-5xl font-bold mb-12'>Vite + React</h1>
-    //     <h3 className='text-neutral-400 text-sm mb-8'>MUHAMMAD LABIB ZAENAL AROFI</h3>
-    //     <div className="text-sm space-y-6 mb-8">
-    //       <button onClick={() => setCount((count) => count + 1)} className='bg-neutral-100 py-2 px-4 rounded-lg text-lg'>
-    //         count is {count}
-    //       </button>
-    //       <p className='text-neutral-500'>
-    //         Edit <code>src/App.jsx</code> and save to test HMR
-    //       </p>
-    //     </div>
-    //   </div>
-    //   <p className="text-neutral-500">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
+    </>
   );
 }
 
-export default App
+export default App;
