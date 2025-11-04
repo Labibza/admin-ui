00668bf270aa0c8318f5eb1538a82144d0857514
Labibza <111202315339@mhs.dinus.ac.id>
2025-11-04 11:48:07 +0700

    UTS

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import PostCard from "./PostCard";
-import postsData from "./assets/postsData.js";
+import postsData from "./assets/postsData.js"; // perhatikan path ini
 
 function Exercise() {
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold text-center mb-6 text-special-red2">
+      <h1 className="text-3xl font-bold text-center mb-6 text-special-red2 font-poppins">
         User Posts
       </h1>
 
--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function PostCard({ id, userId, title, body }) {
-  const [clicked, setClicked] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <div className="flex items-center justify-center">
@@ -12,27 +12,32 @@ function PostCard({ id, userId, title, body }) {
         hover:scale-105 hover:border-gray-300 hover:bg-pink-100
         hover:shadow-lg"
       >
+        {/* Bagian Atas */}
         <h2 className="text-xl font-semibold text-gray-800 mb-3 font-poppins">
           {title}
         </h2>
 
+        {/* Bagian Tengah */}
         <p className="text-gray-700 mb-3 leading-relaxed font-body whitespace-pre-line">
           {body}
         </p>
 
-        <p className="text-gray-500 text-sm">
+        <p className="text-gray-500 text-sm mb-4">
           <strong>Id:</strong> {id} | <strong>User:</strong> {userId}
         </p>
 
+        {/* Bagian Bawah - Tombol */}
         <button
-          onClick={() => setClicked(!clicked)}
-          className=
-          {`mt-4 py-2 rounded-md text-white font-semibold 
+          onClick={() => setIsClicked(!isClicked)}
+          className={`mt-2 py-2 rounded-md text-white font-semibold 
           transition-all duration-300 ease-in-out
           ${
-            clicked? "bg-special-red2 hover:brightness-110": "bg-gray-500 hover:bg-gray-400"}`}
+            isClicked
+              ? "bg-special-red2 hover:brightness-110"
+              : "bg-gray-500 hover:bg-gray-400"
+          }`}
         >
-          {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
+          {isClicked ? "Tombol sudah diklik" : "Silakan Klik"}
         </button>
       </div>
     </div>
