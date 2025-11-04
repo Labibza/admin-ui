b6b92be4151e741d0eb4b8c5a0f19d463fce9540
Labibza <111202315339@mhs.dinus.ac.id>
2025-11-04 11:40:34 +0700

    UTS

---
+++ src/PostCard.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function PostCard({ id, userId, title, body }) {
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="flex flex-col justify-between bg-white p-6 rounded-lg shadow
+        border border-transparent text-center w-96 
+        transition-all duration-300 ease-in-out
+        hover:scale-105 hover:border-gray-300 hover:bg-pink-100
+        hover:shadow-lg"
+      >
+        <h2 className="text-xl font-semibold text-gray-800 mb-3 font-poppins">
+          {title}
+        </h2>
+
+        <p className="text-gray-700 mb-3 leading-relaxed font-body whitespace-pre-line">
+          {body}
+        </p>
+
+        <p className="text-gray-500 text-sm">
+          <strong>Id:</strong> {id} | <strong>User:</strong> {userId}
+        </p>
+
+        <button
+          onClick={() => setClicked(!clicked)}
+          className=
+          {`mt-4 py-2 rounded-md text-white font-semibold 
+          transition-all duration-300 ease-in-out
+          ${
+            clicked? "bg-special-red2 hover:brightness-110": "bg-gray-500 hover:bg-gray-400"}`}
+        >
+          {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default PostCard;
